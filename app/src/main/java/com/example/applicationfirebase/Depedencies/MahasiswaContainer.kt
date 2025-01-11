18eec71818e203f90afef9567ba5a0dc14fe0dfc
farisabqari <127277279+farisabqari@users.noreply.github.com>
2025-01-12 05:30:24 +0800

    container untuk MahasiswaRepository

--- app/src/main/java/com/example/applicationfirebase/Depedencies/MahasiswaContainer.kt
+++ app/src/main/java/com/example/applicationfirebase/Depedencies/MahasiswaContainer.kt
@@ -1,18 +1,10 @@
 package com.example.applicationfirebase.Depedencies
 
-
 import com.example.applicationfirebase.repository.MahasiswaRepository
-import com.example.applicationfirebase.repository.NetworkMahasiswaRespository
+import com.example.applicationfirebase.repository.NetworkMahasiswaRepository
 import com.google.firebase.firestore.FirebaseFirestore
 
-interface AppContainer{
-    val mahasiswaRepository : MahasiswaRepository
+interface AppContainer {
+    val mahasiswaRepository: MahasiswaRepository
 }
 
-class MahasiswaContainer : AppContainer{
-    private  val firebase : FirebaseFirestore = FirebaseFirestore.getInstance() // sejajar dengan URL
-
-    override val mahasiswaRepository: MahasiswaRepository by lazy {
-        NetworkMahasiswaRespository(firebase)
-    }
-}
