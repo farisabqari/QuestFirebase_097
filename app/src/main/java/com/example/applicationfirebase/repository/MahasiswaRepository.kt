34fa69ceb4eca68d03ca5d9a2008633005178e65
farisabqari <127277279+farisabqari@users.noreply.github.com>
2025-01-12 05:27:23 +0800

    membuat MahasiswaRepository untuk mengelola data

--- app/src/main/java/com/example/applicationfirebase/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/applicationfirebase/repository/MahasiswaRepository.kt
@@ -4,13 +4,9 @@ import com.example.applicationfirebase.model.Mahasiswa
 import kotlinx.coroutines.flow.Flow
 
 interface MahasiswaRepository {
-    suspend fun getAllMahasiswa(): Flow<List<Mahasiswa>>
-
+    suspend fun getMahasiswa(): Flow<List<Mahasiswa>>
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
-
-    suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa)
-
-    suspend fun deleteMahasiswa(nim: String)
-
-    suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa>
+    suspend fun getMahasiswaById(nim: String): Flow<Mahasiswa>
+    suspend fun deleteMahasiswa(mahasiswa: Mahasiswa)
+    suspend fun updateMahasiswa(mahasiswa: Mahasiswa)
 }
