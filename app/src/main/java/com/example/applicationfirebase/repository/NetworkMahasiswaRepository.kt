162b90acdd6b1dbfb423bd4b657e2467db893c77
farisabqari <127277279+farisabqari@users.noreply.github.com>
2025-01-12 05:35:28 +0800

    Menambahkan NetworkMahasiswaRepository untuk tambah dan baca data Mahasiswa.

--- app/src/main/java/com/example/applicationfirebase/repository/NetworkMahasiswaRepository.kt
+++ app/src/main/java/com/example/applicationfirebase/repository/NetworkMahasiswaRepository.kt
@@ -1,48 +1,76 @@
 package com.example.applicationfirebase.repository
 
-
 import com.example.applicationfirebase.model.Mahasiswa
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.tasks.await
 
-class NetworkMahasiswaRepository (
+class NetworkMahasiswaRepository(
     private val firestore: FirebaseFirestore
 ) : MahasiswaRepository {
-    override suspend fun getAllMahasiswa(): Flow<List<Mahasiswa>> = callbackFlow {
-
+    override suspend fun getMahasiswa(): Flow<List<Mahasiswa>> = callbackFlow{//mendukung data secara realtime
         val mhsCollection = firestore.collection("Mahasiswa")
-            .orderBy("nim", Query.Direction.DESCENDING)
-            .addSnapshotListener{ value, error ->
+            .orderBy("nama", Query.Direction.ASCENDING)
+            .addSnapshotListener { value, error -> //untuk data listener yang realtime
 
-                if (value != null){
+                //kalau datanya tidak sama dengan null maka data dalam dokumen di masukkan ke model
+                if (value != null) {
                     val mhsList = value.documents.mapNotNull {
                         it.toObject(Mahasiswa::class.java)!!
                     }
-                    trySend(mhsList)
+                    trySend(mhsList) //memberikan fungsi untuk mengirim data ke flow
                 }
             }
         awaitClose{
-            mhsCollection.remove()
+            mhsCollection.remove() //menutup collection
+        }
+    }
+
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        try {
+            firestore.collection("Mahasiswa").add(mahasiswa).await()
+        } catch (e: Exception) {
+            throw Exception ("Gagal menambahkan data mahasiswa: ${e.message}")
+        }
+    }
+
+    override suspend fun updateMahasiswa(mahasiswa: Mahasiswa) {
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .set(mahasiswa)
+                .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal mengupdate data mahasiswa:${e.message}")
         }
     }
 
 
-    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
-
+    override suspend fun deleteMahasiswa(mahasiswa: Mahasiswa) {
+        try {
+            firestore.collection("Mahasiswa")
+                .document(mahasiswa.nim)
+                .delete()
+                .await()
+        } catch (e: Exception) {
+            throw Exception("Gagal menghapus data mahasiswa:${e.message}")
+        }
     }
 
-    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
-
-    }
-
-    override suspend fun deleteMahasiswa(nim: String) {
-
-    }
-
-    override suspend fun getMahasiswaByNim(nim: String): Flow<Mahasiswa> {
-        TODO()
+    override suspend fun getMahasiswaById(nim: String): Flow<Mahasiswa> = callbackFlow {
+        val mhsDocument = firestore.collection("Mahasiswa")
+            .document(nim)
+            .addSnapshotListener { value, error ->
+                if (value != null) {
+                    val mhs = value.toObject(Mahasiswa::class.java)!!
+                    trySend(mhs)
+                }
+            }
+        awaitClose {
+            mhsDocument.remove()
+        }
     }
 }
