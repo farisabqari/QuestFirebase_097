a174879059a33465d2c7f265da1833d043ac43e3
farisabqari <127277279+farisabqari@users.noreply.github.com>
2025-01-13 14:37:28 +0800

    memanggil pengelola halaman untuk menjalankan aplikasi

--- app/src/main/java/com/example/applicationfirebase/MainActivity.kt
+++ app/src/main/java/com/example/applicationfirebase/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.applicationfirebase
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,18 +13,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.applicationfirebase.ui.View.HomeScreen
+import com.example.applicationfirebase.Navigasi.PengelolaHalaman
 import com.example.applicationfirebase.ui.theme.ApplicationFireBaseTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ApplicationFireBaseTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HomeScreen(
-                        navigateToItemEntry = {}, modifier = Modifier.padding(innerPadding)
+                    PengelolaHalaman(
+                        modifier = Modifier.padding(innerPadding),
                     )
                 }
             }
